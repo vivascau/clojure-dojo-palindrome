2ce20e617b209fd3d90224b6018c1b639795b334
vivascau <vlad.ivascau@gmail.com>
2016-03-07 19:46:34 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-dojo-palindrome
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_dojo_palindrome/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-dojo-palindrome.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-dojo-palindrome.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
